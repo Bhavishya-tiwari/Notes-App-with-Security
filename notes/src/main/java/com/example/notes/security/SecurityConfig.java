4ab273d3773e373556834359287e5631bce841fa
Bhavishya-Tiwari <bhavishyatiwari2000@gmail.com>
2024-09-10 22:28:16 +0530

    now using JdbcUserDetailsManager instead of inmemory

--- notes/src/main/java/com/example/notes/security/SecurityConfig.java
+++ notes/src/main/java/com/example/notes/security/SecurityConfig.java
@@ -6,9 +6,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -27,12 +31,12 @@ public class SecurityConfig {
         return http.build();
     }
     @Bean
-    public UserDetailsService userDetailsService () {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+    public UserDetailsService userDetailsService (DataSource datasource) {
+        JdbcUserDetailsManager manager = new JdbcUserDetailsManager(datasource);
         if (!manager.userExists("user1")) {
             manager.createUser(
                     User.withUsername("user1")
-                            .password("(noop}password1")
+                            .password("{noop}password1")
                             .roles("USER")
                             .build()
             );
